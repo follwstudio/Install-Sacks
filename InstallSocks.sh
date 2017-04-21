20bea6b2fec510afa2cf84bc78598c88ad2ba96e
follwstudio <git@follw.me>
2017-04-21 08:40:53 +0800

    OK

---
+++ InstallSocks.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget --no-check-certificate -O shadowsocks-go.sh https://raw.githubusercontent.com/malaohu/ruyo-shell/master/shadowsocks-go.sh
+chmod +x shadowsocks-go.sh
+./shadowsocks-go.sh 2>&1
